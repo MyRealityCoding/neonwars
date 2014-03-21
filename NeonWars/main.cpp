8d1005bee15d134a0d7ea7fbb60e2172d0b4e10e
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-03-21 10:21:51 +0100

    Added stuff

--- NeonWars/main.cpp
+++ NeonWars/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QGraphicsScene scene;
+    scene.setSceneRect(QRect(0, 0, 200, 200));
     QGraphicsView view(&scene);
-
     QGraphicsPixmapItem *item = new QGraphicsPixmapItem(QPixmap(":BACKGROUND"));
 
     scene.addItem(item);
