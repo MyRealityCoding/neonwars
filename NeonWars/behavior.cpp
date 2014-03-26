f40456864c11b47f3ed3803b2c7a588c938b9b36
Jan Kerkenhoff <j.kerkenhoff@student.fontys.nl>
2014-03-27 00:40:25 +0100

    Added Behaviour

--- NeonWars/behavior.cpp
+++ NeonWars/behavior.cpp
@@ -2,7 +2,55 @@
 #include "ship.h"
 #include "board.h"
 
+
 void NormalBehavior::trigger(Board *board, const int &x, const int &y)
 {
 
+}
+
+void  LaserBehavior::trigger(Board *board, const int &x, const int &y)
+{
+    std::vector<QPoint> vector = board->getShips(x,y,Neighbour::UPPER, Neighbour::LOWER);
+    foreach (QPoint var, vector) {
+         board->remove(var.x(),var.y());
+    }
+}
+void BombBehavior::trigger(Board *board, const int &x, const int &y)
+{
+    std::vector<QPoint> vector = board->getNeighbours(x,y);
+    foreach (QPoint var, vector) {
+         board->remove(var.x(),var.y());
+    }
+
+}
+
+void RocketBehavior::trigger(Board *board, const int &x, const int &y)
+{
+    Player::Type player(board->getShip(x,y)->getPlayer());
+    std::vector<QPoint> vector = board->getNeighbours(x,y);
+    foreach (QPoint var, vector) {
+        if(board->getShip(var.x(),var.y())->getPlayer() == !player){
+            board->remove(var.x(),var.y());
+        }
+    }
+}
+
+void PhaserBehavior::trigger(Board *board, const int &x, const int &y)
+{
+    Player::Type player(board->getShip(x,y)->getPlayer());
+    //Get Diagonal 1
+    std::vector<QPoint> vector1 = board->getShips(x,y,Neighbour::UPPER_LEFT, Neighbour::LOWER_RIGHT);
+    foreach (QPoint var, vector1) {
+        if(board->getShip(var.x(),var.y())->getPlayer() == !player){
+            board->remove(var.x(),var.y());
+        }
+    }
+    // Get Diagonal 2
+
+    std::vector<QPoint> vector2 = board->getShips(x,y,Neighbour::LOWER_LEFT, Neighbour::UPPER_RIGHT);
+    foreach (QPoint var, vector2) {
+        if(board->getShip(var.x(),var.y())->getPlayer() == !player){
+            board->remove(var.x(),var.y());
+        }
+    }
 }
--- NeonWars/behavior.h
+++ NeonWars/behavior.h
@@ -16,4 +16,28 @@ public:
     void trigger(Board *board, const int&, const int&);
 };
 
+class LaserBehavior: public Behavior {
+    void trigger(Board *board, const int&, const int&);
+};
+
+class BombBehavior: public Behavior {
+    void trigger(Board *board, const int&, const int&);
+};
+class RocketBehavior: public Behavior {
+    void trigger(Board *board, const int&, const int&);
+};
+class PhaserBehavior: public Behavior {
+    void trigger(Board *board, const int&, const int&);
+};
+
+
+
+
+//        NONE ,  // EMPTY FIELD TYPE
+//        NORMAL, // Does nothing
+//        LASER,  // Kills all Ships beneath it
+//        BOMB,   //Kills all neighbour Ships
+//        ROCKET, // Kills all neighbour enemy Ships
+//        PHASER
+
 #endif // BEHAVIOR_H
